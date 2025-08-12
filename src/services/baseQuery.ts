02bdb691449d3a81893e22f4ef6a90ce6ea773c7
audiputra19 <audiputra19@gmail.com>
2025-08-12 08:56:35 +0700

    update base query

--- src/services/baseQuery.ts
+++ src/services/baseQuery.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseQuery = fetchBaseQuery({
-    // baseUrl: "https://api-koperasi-psi.vercel.app"
-    baseUrl: "http://localhost:3001"
+    baseUrl: "https://api-koperasi-psi.vercel.app"
+    // baseUrl: "http://localhost:3001"
 });
