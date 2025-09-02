cf624a74ae37ada90020067f5b2039132f9444ad
audiputra19 <audiputra19@gmail.com>
2025-09-02 07:39:57 +0700

    update persist gate

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { store } from './store/index.ts'
+import { persistor, store } from './store/index.ts'
+import { PersistGate } from 'redux-persist/integration/react'
+import Loading from './components/Loading.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<Loading/>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
